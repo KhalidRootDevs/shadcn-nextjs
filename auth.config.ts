440068a815f2346c00c46648426a21f0e3686d68
khalidrootdevs <khalidrootdevs@gmail.com>
2024-07-09 23:38:39 +0600

    google auth added

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,13 @@
 import { NextAuthConfig } from 'next-auth';
 import CredentialProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
-
+import GoogleProvider from 'next-auth/providers/google';
 const authConfig = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? '',
       clientSecret: process.env.GITHUB_SECRET ?? ''
